45a32532df14cf91232232c3420680fa1c6b1fef
Vlad Moskalenko <vladmoskalenko@MacBook-Air-Vlad.local>
2025-06-26 20:49:11 +0300

    Исключил login.py из Git

--- login.py
+++
@@ -1,7 +0,0 @@
-from telethon.sync import TelegramClient
-
-api_id = 29873749
-api_hash = 'f747a66e89ef4b31e6ca7b7d06445091'
-
-with TelegramClient('session', api_id, api_hash) as client:
-    print("✅ Успешный вход как:", client.get_me().first_name)
